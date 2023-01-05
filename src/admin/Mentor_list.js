6ba3a5868171cd10e4fec119766fb4341163d1c5
JagadeeswaranG <jagadees.vg@gmail.com>
2023-01-05 16:26:32 +0530

    loading

--- src/admin/Mentor_list.js
+++ src/admin/Mentor_list.js
@@ -5,6 +5,7 @@ import { listMentors } from "../api/mentors";
 // All Mentors list for Admin
 function Mentor_list() {
   const [mentors, setMentors] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     getMentors();
@@ -12,8 +13,10 @@ function Mentor_list() {
 
   async function getMentors() {
     try {
+      setLoading(true);
       let mentorsData = await listMentors();
       setMentors(mentorsData.data);
+      setLoading(false);
     } catch (error) {
       alert(error.response.data.message);
     }
@@ -21,44 +24,54 @@ function Mentor_list() {
   return (
     <div class="container-fluid">
       <h1 class="h3 mb-4 text-gray-800">Mentor's Details</h1>
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <Link to={"/admin_portal/mentors/add"} type="button" class="btn btn-outline-primary">
-            Add Mentor
-          </Link>
+      {isLoading ? (
+        <div class="spinner-border text-dark" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table
-              class="table table-bordered"
-              id="dataTable"
-              width="100%"
-              cellspacing="0"
+      ) : (
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+            <Link
+              to={"/admin_portal/mentors/add"}
+              type="button"
+              class="btn btn-outline-primary"
             >
-              <thead>
-                <tr>
-                  <th>Mentor Id</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Mobile No.</th>
-                </tr>
-              </thead>
-              <tbody>
-                {mentors.map((item,index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{item._id}</td>
-                      <td>{item.name}</td>
-                      <td>{item.email}</td>
-                      <td>{item.phone}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+              Add Mentor
+            </Link>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table
+                class="table table-bordered"
+                id="dataTable"
+                width="100%"
+                cellspacing="0"
+              >
+                <thead>
+                  <tr>
+                    <th>Mentor Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile No.</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mentors.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{item._id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
