097884941825b1a12c5b9b9b2394ce3ff41e63b2
MManimekalai <manimekalai.eee123@gmail.com>
2023-08-14 15:01:09 +0530

    Initial commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    api : "https://zenqueryticket-backend.onrender.com",//http://localhost:3000
+    api : "https://zenclassticketing-backend.onrender.com/",//http://localhost:3000
     storage_key : "zen_ticket"
 }
